e028980bf556c7a8fada28fd436222897c700c9e
mr5z <dev.kram5@gmail.com>
2026-07-28 21:27:23 +0800

    Remove unused code

--- src/Extensions/NavigationServiceExtension.cs
+++ src/Extensions/NavigationServiceExtension.cs
@@ -8,8 +8,6 @@ using Nkraft.MvvmEssentials.ViewModels;
 // ReSharper disable once CheckNamespace
 namespace Nkraft.MvvmEssentials;
 
-public interface INavigationTarget;
-
 public static class NavigationExtension
 {
 	extension(INavigationService navigationService)
@@ -22,17 +20,6 @@ public static class NavigationExtension
 		/// <returns></returns>
 		public Task<IResult> NavigateAsync(PageDestination pageDestination, bool animated = true)
 			=> navigationService.NavigateAsync(pageDestination.PageName, pageDestination.Parameters, animated);
-		
-		public async Task<IResult> NavigateAsync<TViewModel>(
-			INavigationTarget parameters, bool animated = true)
-			where TViewModel : PageViewModel
-		{
-			var pageName = PageHelper.ToPageName<TViewModel>("Page");
-			var navParam = new NavigationParameters();
-			foreach (var (key, value) in ObjectHelper.ToDictionary(parameters))
-				navParam.Add(key, value);
-			return await navigationService.NavigateAsync(pageName, navParam, animated);
-		}
 		
 		/// <summary>
 		/// Navigates to the page associated with the specified ViewModel type, passing parameters as a strongly-typed object.
