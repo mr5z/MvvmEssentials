e482534ee5efd649dc6bca0edccc37a627f56516
mr5z <dev.kram5@gmail.com>
2026-07-31 01:51:04 +0800

    Update comment

--- src/Services/AppStartup.cs
+++ src/Services/AppStartup.cs
@@ -5,7 +5,7 @@ namespace Nkraft.MvvmEssentials.Services;
 /// <summary>
 /// Defines the application startup contract. Implement this anywhere in your project
 /// to control initial navigation. If not implemented, the page marked with
-/// <c>isInitial: true</c> in <c>AddPageRegistry</c> will be used automatically.
+/// <c>isInitial: true</c> in <c>MapPage</c> will be used automatically.
 /// </summary>
 public interface IAppStartup
 {
