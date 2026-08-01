53a0b6a8c7ba45089cdcaa9c895f493275ded706
mr5z <dev.kram5@gmail.com>
2026-08-02 02:29:37 +0800

    Remove unused package

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -2,5 +2,3 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Nkraft.MvvmEssentials.UnitTest")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
-
-[assembly: PropertyChanged.FilterType("Nkraft.MvvmEssentials.ViewModels.")]
